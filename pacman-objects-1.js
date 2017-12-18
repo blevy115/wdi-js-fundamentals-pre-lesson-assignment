50d1180fe289d4c4095ea1c8abaf6516996d5fb6
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-12-17 22:18:45 -0500

    completed Object collection js for ghosts

---
+++ pacman-objects-1.js
@@ -0,0 +1,6 @@
+// pacman-objects-1.js
+
+var inky = {};
+inky.colour = 'Cyan';
+inky.personality = 'Shadow';
+console.log(inky);
---
+++ pacman-objects-2.js
@@ -0,0 +1,15 @@
+// pacman-objects-2.js
+
+var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange'};
+console.log(ghosts);
+
+console.log(ghosts.inky);
+console.log(ghosts.blinky);
+console.log(ghosts.pinky);
+console.log(ghosts.clyde);
+
+console.log(ghosts['inky']);
+console.log(ghosts['blinky']);
+
+var ghostName = 'clyde'
+console.log(ghosts[ghostName]);
